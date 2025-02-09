8e5e49e7ea80fe6d97501b1d524656e4d6086fe8
yakimka <ss.yakim@gmail.com>
2025-02-09 15:35:50 +0200

    update run_wrk default parameters

--- run_wrk.sh
+++ run_wrk.sh
@@ -2,20 +2,15 @@
 
 if [ -z "$1" ]; then
   echo "Error: Missing name."
-  echo "Usage: $0 <name>"
+  echo "Usage: $0 <name> [host] [duration] [threads] [connections]"
   exit 1
 fi
 
 name="$1"
 host="${2:-http://127.0.0.1:8000}"
 duration="${3:-600}"
-threads="${4:-4}"
-
-if [ -z "$5" ]; then
-  echo "Error: Missing connections count."
-  exit 1
-fi
-connections="$5"
+threads="${4:-8}"
+connections="${5:-200}"
 
 echo "Warming up for 10 seconds"
 wrk -t 4 -c 40 -d 10 $host/plaintext > /dev/null 2>&1
